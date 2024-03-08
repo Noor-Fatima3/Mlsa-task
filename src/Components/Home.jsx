36b75cddeebbc948fe6b942ece788391ff42a0a9
Noor-Fatima3 <162204005+Noor-Fatima3@users.noreply.github.com>
2024-03-08 21:54:02 +0500

    Update image in home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Backgrooung.jpg";
+import image from "../images/Backgrounf.jpg";
 
 const imageAltText = "Blank backgrounf black photo esthetic";
 
