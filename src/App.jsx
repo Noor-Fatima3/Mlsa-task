099861ab586974bd7b751c1fe32b600d34402032
Noor-Fatima3 <162204005+Noor-Fatima3@users.noreply.github.com>
2024-03-08 20:55:52 +0500

    update info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Noor Fatima",
+  title: "Student",
+  email: "Noorfatima1122330@outlook.com",
+  gitHub: "https://github.com/Noor-Fatima3",
 };
 
 const primaryColor = "#4E567E";
